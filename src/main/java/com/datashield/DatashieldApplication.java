3d3a33c5566891ae4d1176f8c130547719491626
fishshi <2855691008@qq.com>
2025-09-01 09:19:27 +0800

    init

---
+++ src/main/java/com/datashield/DatashieldApplication.java
@@ -0,0 +1,13 @@
+package com.datashield;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatashieldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatashieldApplication.class, args);
+	}
+
+}
